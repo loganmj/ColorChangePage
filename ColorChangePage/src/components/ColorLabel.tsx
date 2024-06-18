b8f44ce59a96260b57c4748a809ad2bf8f29dfb7
Michael <michaellogan7860@gmail.com>
2024-06-18 14:06:34 -0500

    Changed label text from "{color}" to "Current Color: {color}"

--- ColorChangePage/src/components/ColorLabel.tsx
+++ ColorChangePage/src/components/ColorLabel.tsx
@@ -5,7 +5,7 @@ const ColorLabel: React.FC = () => {
   // Use context for color property
   const { colorText } = useColorContext();
 
-  return <label className="color-label">{colorText}</label>;
+  return <label className="color-label">Current Color: {colorText}</label>;
 };
 
 export default ColorLabel;
