807fe9720fc807ee93a0e8309bc548618fa03ad1
Michael <michaellogan7860@gmail.com>
2024-06-18 13:53:21 -0500

    Added ColorLabel.css

--- ColorChangePage/src/components/ColorLabel.tsx
+++ ColorChangePage/src/components/ColorLabel.tsx
@@ -1,10 +1,11 @@
 import useColorLabelContext from "./ColorLabelHook";
+import "../styles/ColorLabel.css";
 
 const ColorLabel: React.FC = () => {
   // Use context for color property
   const { colorText } = useColorLabelContext();
 
-  return <label>{colorText}</label>;
+  return <label className="color-label">{colorText}</label>;
 };
 
 export default ColorLabel;
