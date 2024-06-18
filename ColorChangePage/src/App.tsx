44add44a7637b41c83029c22e8175de37a95374c
Michael <michaellogan7860@gmail.com>
2024-06-18 11:20:50 -0500

    Fixed the app class.

--- ColorChangePage/src/App.tsx
+++ ColorChangePage/src/App.tsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import useBackgroundColor from "./components/BackgroundColorHook";
+import BackgroundColorProvider from "./components/BackgroundColorProvider";
+import ColorButtons from "./components/ColorButtons";
 
-const App = () => {
+const App: React.FC = () => {
   const { color } = useBackgroundColor();
 
-  return{
+  return (
     <BackgroundColorProvider>
-      <div style={{backgroundColor: color, height: '100vh', width: '100vw'}}>
-        <ColorButtons/>
+      <div style={{ backgroundColor: color, height: "100vh", width: "100vw" }}>
+        <ColorButtons />
       </div>
     </BackgroundColorProvider>
-  };
+  );
 };
 
 export default App;
