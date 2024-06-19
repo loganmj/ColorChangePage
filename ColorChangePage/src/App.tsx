bf814405cd6d059aece61673b38e78fb5c3b315c
Michael <michaellogan7860@gmail.com>
2024-06-19 08:28:06 -0500

    Added some adjustments based on viewport size.

--- ColorChangePage/src/App.tsx
+++ ColorChangePage/src/App.tsx
@@ -5,16 +5,18 @@ import ColorLabel from "./components/ColorLabel";
 
 const App: React.FC = () => {
   return (
-    <ColorLabelContextProvider>
-      <div className="buttons-div">
-        <ColorButton text="red"></ColorButton>
-        <ColorButton text="green"></ColorButton>
-        <ColorButton text="blue"></ColorButton>
-      </div>
-      <div className="label-div">
-        <ColorLabel />
-      </div>
-    </ColorLabelContextProvider>
+    <div className="app-context">
+      <ColorLabelContextProvider>
+        <div className="buttons-div">
+          <ColorButton text="red"></ColorButton>
+          <ColorButton text="green"></ColorButton>
+          <ColorButton text="blue"></ColorButton>
+        </div>
+        <div className="label-div">
+          <ColorLabel />
+        </div>
+      </ColorLabelContextProvider>
+    </div>
   );
 };
 
